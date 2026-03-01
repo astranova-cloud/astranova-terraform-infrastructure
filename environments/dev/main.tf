04cdfb136ebe16a61d1a37544ce0d0c42db5165e
ROHIT KUMAR MEHER <meherrohit99@gmail.com>
2026-03-01 20:06:58 +0530

    Deploy dev VPC module and update gitignore

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,26 @@
-# DEV environment Terraform entry point
+provider "aws" {
+  region = "us-east-1"
+}
 
-# Infrastructure modules will be called from here.
+module "vpc" {
+  source = "../../modules/VPC"
+
+  environment = "dev"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
