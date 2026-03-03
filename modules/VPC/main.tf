c817013a4a3f5cbb8dedaf21d865826ede1bfdc8
ROHIT KUMAR MEHER <meherrohit99@gmail.com>
2026-03-03 12:55:55 +0530

    Modified EKS,VPC MODULE,INGRESS YAML

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -159,7 +159,3 @@ resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
-module "iam" {
-  source      = "../../modules/iam"
-  environment = "dev"
-}
